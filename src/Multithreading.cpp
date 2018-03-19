5b71b40f6e35ced1482ad8be5437b723c898859b
Aditya Deorha <aditya.deorha1@gmail.com>
2018-03-19 00:36:31 -0700

    Using STL with multithreading

--- src/Multithreading.cpp
+++ src/Multithreading.cpp
@@ -1,23 +1,17 @@
-//============================================================================
-// Name        : Multithreading.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 #include <thread>
+#include <vector>
 using namespace std;
 
-void function_1()
-{
-	cout<<"function_1\n";
+void function_1(vector<int> i1){
+	cout<<"function_1 Size of i1 = "<<i1.size()<<endl;
 }
 
-int main() {
-	thread t1(function_1);
+int main(){
+	vector<int> i2 = {1, 2, 3, 4};
+	thread t1 {function_1, i2};
+
 	t1.join();
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
